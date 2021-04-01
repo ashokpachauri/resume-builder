ae06a562ea127fd81f9b38b3303aca1c1f3822dd
ashokpachauri <pachauriashokkumar@gmail.com>
2021-04-01 21:57:25 +0530

    Added References to builder

--- src/constant/api.js
+++ src/constant/api.js
@@ -6,7 +6,7 @@
 export default {
     // The URL we're connecting to
     // hostname: 'http://localhost:3007',
-    hostname: '161.117.86.8',
+    hostname: 'http://161.117.86.8',
     imgurHostname: 'https://api.imgur.com/3/image',
 
     endpoints: [
